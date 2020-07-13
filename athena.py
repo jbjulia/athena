b7601e5d96a1e2cc90f5c9f4fc1f1627fb17f247
Joseph Julian <57590612+jbjulia@users.noreply.github.com>
2020-07-13 11:50:49 +0100

    Create athena.py

---
+++ athena.py
@@ -0,0 +1,12 @@
+"""
+This project is a collaborative effort to automate the operations of
+the FIOC UK Battle Watch. This desktop application is a feature-rich
+tool suite that intelligently tracks user activity, provides efficient
+report dissemination functions, and allows for visually-aided vessel
+tracking. If you are interested in viewing this project, please access
+GitLab from any NSANET position, where you can find this repository
+under 'jbjulia/athena'. At this time, Athena is currently being
+reviewed for transfer, and will soon be accessible from NIPRNET for
+demonstration purposes only. For more information, please contact me at
+jbjulia@nsa.ic.gov, or joseph.b.julian@gmail.com for non-NSA interest.
+"""
